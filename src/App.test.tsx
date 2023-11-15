2785ab4408830311f62aa68ce6945b97832701e0
DmitriyKaplan28 <101748283+DmitriyKaplan28@users.noreply.github.com>
2023-11-15 18:32:11 +0200

    Update App.test.tsx

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,7 +3,6 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 
-//mobile test
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
